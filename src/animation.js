a43170b00f0fe825518b12757d0ab63190622fd0
sowsouleymane <souleymane.sow@eurostar.com>
2020-12-28 08:18:59 +0000

    finish AboutSection animation

--- src/animation.js
+++ src/animation.js
@@ -4,19 +4,62 @@ export const pageAnimation = {
     y: 300
   },
   show: {
-      opacity: 1,
-      y: 0,
-      transition:{
-          duration:1,
-
-      }
-    },
-  exit: {
-      opacity: 0,
-      y: 300,
-      transition:{
-          duration:1,
-
-      }
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      when: "beforeChildren",
+      staggerChildren: 0.25
     }
+  },
+  exit: {
+    opacity: 0,
+    y: 300,
+    transition: {
+      duration: 0.25
+    }
+  }
+};
+
+export const titleAnim = {
+  hidden: {
+    y: 200
+  },
+  show: {
+    y: 0,
+    transition: {
+      duration: 0.75,
+      ease: "easeOut"
+    }
+  }
+};
+
+export const fade = {
+  hidden: {
+    opacity: 0
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: "easeOut",
+      duration: 0.75
+    }
+  }
+};
+
+export const photoAnim = {
+  hidden: {
+    scale: 1.5,
+    opacity: 0
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      scale: 1,
+      ease: "easeOut",
+      duration: 0.75
+    }
+  }
 };
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import home1 from "../img/home1.png";
 import {About,Description,Image,Hide} from '../styles';
 import {motion} from 'framer-motion';
+import {titleAnim,fade,photoAnim} from '../animation';
 
 const AboutSection = () => {
   return (
@@ -9,27 +10,27 @@ const AboutSection = () => {
       <Description>
         <motion.div className="title">
           <Hide>
-            <motion.h2>We work to make </motion.h2>
+            <motion.h2 variants={titleAnim} >We work to make </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim} >
               your <span>dreams</span> come{" "}
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true. </motion.h2>
+            <motion.h2 variants={titleAnim} >true. </motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade} >
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ipsa
           quidem qui blanditiis deleniti expedita aperiam voluptate, similique,
           quo sequi quod non? Et quia exercitationem ducimus, numquam atque
           delectus accusamus.
-        </p>
-        <button>Contact us</button>
+        </motion.p>
+        <motion.button variants={fade} >Contact us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </Image>
     </About>
   );
